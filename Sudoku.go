6b33a5e484f04738f7be60df1b52b07654947e51
Murray Christopherson <murray.christopherson@hipplay.com>
2014-04-01 10:26:38 -0400

    Working on the generation algorithm.

--- Sudoku.go
+++ Sudoku.go
@@ -1,46 +1,127 @@
 // Sudoku project Sudoku.go
 package Sudoku
 
-import ()
-
-const (
-	pHeight = 3
-	height  = pHeight * 3
-	pWidth  = 3
-	width   = pWidth * 3
+import (
+	"math/rand"
 )
 
-func Generate() [][]int8 {
+const (
+	boardSize        = 9
+	subdivisions     = 3
+	pHeight          = boardSize / subdivisions
+	height           = boardSize
+	pWidth           = boardSize / subdivisions
+	width            = boardSize
+	DefaultNumBlanks = 30
+)
+
+func Generate() [][]int {
+	return GenerateWithBlanks(DefaultNumBlanks)
+}
+
+func GenerateWithBlanks(numBlanks int) [][]int {
 	final := generateFinal()
-	retVal := generatePartial(final)
+	retVal := generatePartial(final, numBlanks)
 	return retVal
 }
 
-func generatePartial(final [][]int8) [][]int8 {
-	retVal := make([][]int8, width)
-	for i := 0; i < width; i++ {
-		retVal[i] = make([]int8, height)
-		for j := 0; j < height; j++ {
-			retVal[i][j] = -1
+func generatePartial(final [][]int, numBlanks int) [][]int {
+
+	var numSpaces = 0
+	retVal := make([][]int, len(final))
+	for i := 0; i < len(final); i++ {
+		retVal[i] = make([]int, len(final[i]))
+		numSpaces += len(final[i])
+	}
+
+	fillWithNulls := numBlanks < (numSpaces / 2)
+
+	if fillWithNulls {
+		for i := 0; i < len(retVal); i++ {
+			copy(retVal[i], final[i])
+		}
+	}
+
+	for i := 0; i < numBlanks; {
+		randX := rand.Intn(len(retVal))
+		randCol := retVal[randX]
+		randY := rand.Intn(len(randCol))
+		randVal := randCol[randY]
+		if fillWithNulls {
+			if randVal > 0 {
+				retVal[randX][randY] = 0
+				i++
+			}
+		} else {
+			if randVal < 1 {
+				retVal[randX][randY] = final[randX][randY]
+				i++
+			}
 		}
 	}
 	return retVal
 }
 
-func generateFinal() [][]int8 {
-	retVal := make([][]int8, width)
+func generateFinal() [][]int {
+	retVal := make([][]int, width)
 	for i := 0; i < width; i++ {
-		retVal[i] = make([]int8, height)
-		for j := 0; j < width; j++ {
-			retVal[i][j] = -1
-		}
+		retVal[i] = make([]int, height)
 	}
 
 	for i := 0; i < width; i++ {
 		for j := 0; j < height; j++ {
-			// TODO usableNumbers :=
+			usableNumbers := goodNumbers(i, j, retVal)
+			retVal[i][j] = usableNumbers[rand.Intn(len(usableNumbers))]
 		}
 	}
 
+	return retVal
+}
+
+func goodNumbers(x int, y int, currentBoard [][]int) []int {
+	// TODO handle holders
+	var squareHolder = make([]int, 0, boardSize)
+	for i := 1; i <= boardSize; i++ {
+		squareHolder = append(squareHolder, i)
+	}
+	var lineHolderV = make([]int, 0, boardSize)
+	for i := 1; i <= boardSize; i++ {
+		lineHolderV = append(lineHolderV, i)
+	}
+	lineHolderH := make([]int, 0, boardSize)
+	for i := 1; i <= boardSize; i++ {
+		lineHolderH = append(lineHolderH, i)
+	}
+
+	var retVal = make([]int, 0, boardSize)
+	for i := 1; i <= boardSize; i++ {
+		var useValue = true
+		for j := 0; j < len(squareHolder); j++ {
+			if squareHolder[j] == i {
+				useValue = false
+				break
+			}
+		}
+		if useValue {
+			for j := 0; j < len(lineHolderV); j++ {
+				if lineHolderV[j] == i {
+					useValue = false
+					break
+				}
+			}
+		}
+		if useValue {
+			for j := 0; j < len(lineHolderH); j++ {
+				if lineHolderH[j] == i {
+					useValue = false
+					break
+				}
+			}
+		}
+
+		if useValue {
+			retVal = append(retVal, i)
+		}
+	}
 	return retVal
 }
